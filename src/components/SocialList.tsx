85c10a037f86ee5cdef564d7c286f19f67dd80ad
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-08-13 10:58:57 +0200

    Add relevant some icon-link, remove redundant

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,26 +1,17 @@
 import React from "react";
-import Twitter from "../assets/twitter-alt.svg";
-import GitHub from "../assets/github-alt.svg";
+import FaceBook from "../assets/facebook-alt.svg";
 import config from "../lib/config";
 
 export function SocialList({}) {
   return (
     <div>
       <a
-        title="Twitter"
-        href={`https://twitter.com/${config.twitter_account}`}
+        title="Facebook"
+        href={`https://facebook.com/${config.facebook_account}`}
         target="_blank"
         rel="noopener"
       >
-        <Twitter width={24} height={24} fill={"#222"} />
-      </a>
-      <a
-        title="GitHub"
-        href={`https://github.com/${config.github_account}`}
-        target="_blank"
-        rel="noopener"
-      >
-        <GitHub width={24} height={24} fill={"#222"} />
+        <FaceBook width={24} height={24} fill={"#222"} />
       </a>
       <style jsx>{`
         a {
