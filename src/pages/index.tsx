228836d21de22edde5b5158907795b477708ad8d
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-08-13 11:08:13 +0200

    Remove twittercardmeta to test for build failure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../components/meta/TwitterCardMeta";
+
 import { SocialList } from "../components/SocialList";
 import Traits from "../assets/traits-01.svg"
 
@@ -10,7 +10,7 @@ export default function Index() {
     <Layout>
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
-      <TwitterCardMeta url={"/"} />
+
       <div className="container">
         <div>
           <h1>
